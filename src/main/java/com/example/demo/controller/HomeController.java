389465c66b00d93f5f4f6d9d432de0d93a544566
Kiet Le <kiet.le@hansencx.com>
2021-06-10 20:26:15 +0700

    Prevent go to login page if user is authenticated

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,10 +2,6 @@ package com.example.demo.controller;
 
 import java.security.Principal;
 
-import javax.annotation.Resource;
-import javax.servlet.Filter;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
@@ -15,17 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.dto.AccountDTO;
 import com.example.demo.security.model.SecurityAccount;
-import com.example.demo.service.IAccountService;
 import com.example.demo.view.model.LoginModel;
 
 @Controller
 public class HomeController {	
-	@Autowired
-	private IAccountService accountServ;
-	
-	@Resource
-	Filter springSessionRepositoryFilter;
-	
 	@GetMapping("/index")
 	public String index(@RequestParam (value = "name", required = false, defaultValue = "boos") String name, Model model, Principal principal) {
 		SecurityAccount account = (SecurityAccount) ((UsernamePasswordAuthenticationToken)principal).getPrincipal();
@@ -36,7 +25,14 @@ public class HomeController {
 	}
 	
 	@GetMapping("/login")
-	public String login (Model model) {
+	public String login (Model model,  Principal principal) {
+		if (principal == null) {
+			return "login";
+		}
+		SecurityAccount account = (SecurityAccount) ((UsernamePasswordAuthenticationToken)principal).getPrincipal();
+		if(account.isAccountNonExpired()) {
+			return String.format("redirect:/index?name=%s", account.getUsername());
+		}
 		model.addAttribute("loginForm",new LoginModel());
 		return "login";
 	}
