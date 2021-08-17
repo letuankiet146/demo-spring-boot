8e279189f40886a9711726549fe4c0e26cbc8a19
Kiet Le <kiet.le@hansencx.com>
2021-08-18 04:35:27 +0700

    Limit accessing scope of the user

--- src/main/java/com/example/demo/controller/HomeRestController.java
+++ src/main/java/com/example/demo/controller/HomeRestController.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +20,7 @@ import com.example.demo.dto.AccountDTO;
 import com.example.demo.dto.CommentDTO;
 import com.example.demo.entity.Account;
 import com.example.demo.entity.Comment;
+import com.example.demo.security.model.SecurityAccount;
 import com.example.demo.service.IAccountService;
 import com.example.demo.service.ICommentService;
 import com.example.demo.view.model.FindAccountReq;
@@ -57,7 +63,9 @@ public class HomeRestController {
 	}
 	
 	@PostMapping("/account")
-	public AccountDTO getAccountInfo(@RequestBody FindAccountReq body) {
+	@PostAuthorize ("returnObject.user == authentication.name")
+	public AccountDTO getAccountInfo(@RequestBody FindAccountReq body, Principal principal) {
+		
 		String type = body.getType();
 		String value = body.getValue();
 		switch (type) {
